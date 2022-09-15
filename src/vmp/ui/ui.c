935742380efe2f109fb3a1caa63b4ae4e192ec66
Milan Toth <milgra@milgra.com>
2022-09-15 19:42:05 +0000

    added song list table

--- src/vmp/ui/ui.c
+++ src/vmp/ui/ui.c
@@ -20,8 +20,11 @@ void ui_describe();
 #include "coder.c"
 #include "config.c"
 #include "tg_css.c"
+#include "tg_text.c"
 #include "ui_compositor.c"
 #include "ui_manager.c"
+#include "ui_table.c"
+#include "ui_util.c"
 #include "vh_button.c"
 #include "vh_key.c"
 #include "viewgen_css.c"
@@ -32,6 +35,7 @@ void ui_describe();
 #include "zc_callback.c"
 #include "zc_cstring.c"
 #include "zc_log.c"
+#include "zc_number.c"
 #include "zc_path.c"
 #include "zc_text.c"
 
@@ -42,6 +46,8 @@ struct _ui_t
 
     view_t* exit_btn;
     view_t* full_btn;
+
+    ui_table_t* songlisttable;
 } ui;
 
 void ui_on_key_down(void* userdata, void* data)
@@ -56,6 +62,42 @@ void ui_on_btn_event(void* userdata, void* data)
 
     if (btnview == ui.exit_btn) wm_close();
     if (btnview == ui.full_btn) wm_toggle_fullscreen();
+}
+
+void on_songlist_event(ui_table_t* table, ui_table_event event, void* userdata)
+{
+    switch (event)
+    {
+	case UI_TABLE_EVENT_FIELDS_UPDATE:
+	{
+	    zc_log_debug("fields update %s", table->id);
+	}
+	break;
+	case UI_TABLE_EVENT_SELECT:
+	{
+	    zc_log_debug("select %s", table->id);
+	}
+	break;
+	case UI_TABLE_EVENT_OPEN:
+	{
+	    zc_log_debug("open %s", table->id);
+	}
+	break;
+	case UI_TABLE_EVENT_DRAG:
+	{
+	    zc_log_debug("drag %s", table->id);
+	}
+	break;
+	case UI_TABLE_EVENT_KEY:
+	{
+	    zc_log_debug("key %s", table->id);
+	}
+	break;
+	case UI_TABLE_EVENT_DROP:
+	    zc_log_debug("drop %s", table->id);
+
+	    break;
+    }
 }
 
 void ui_add_cursor()
@@ -159,6 +201,60 @@ void ui_init(float width, float height)
 
     ui.full_btn = view_get_subview(ui.view_base, "app_maximize_btn");
     vh_button_add(ui.full_btn, VH_BUTTON_NORMAL, btn_cb);
+
+    /* songlist */
+
+    vec_t* fields = VNEW();
+    VADDR(fields, cstr_new_cstring("file/basename"));
+    VADDR(fields, num_new_int(400));
+    /* VADDR(fields, cstr_new_cstring("file/mime")); */
+    /* VADDR(fields, num_new_int(200)); */
+    /* VADDR(fields, cstr_new_cstring("file/path")); */
+    /* VADDR(fields, num_new_int(200)); */
+    VADDR(fields, cstr_new_cstring("file/size"));
+    VADDR(fields, num_new_int(120));
+    VADDR(fields, cstr_new_cstring("file/last_access"));
+    VADDR(fields, num_new_int(180));
+    VADDR(fields, cstr_new_cstring("file/last_modification"));
+    VADDR(fields, num_new_int(180));
+    VADDR(fields, cstr_new_cstring("file/last_status"));
+    VADDR(fields, num_new_int(180));
+
+    view_t* songlist       = view_get_subview(ui.view_base, "songlisttable");
+    view_t* songlistscroll = view_get_subview(ui.view_base, "songlistscroll");
+    view_t* songlistevt    = view_get_subview(ui.view_base, "songlistevt");
+    view_t* songlisthead   = view_get_subview(ui.view_base, "songlisthead");
+
+    textstyle_t ts = ui_util_gen_textstyle(songlist);
+
+    ts.align        = TA_CENTER;
+    ts.margin_right = 0;
+    ts.size         = 60.0;
+    ts.textcolor    = 0x353535FF;
+    ts.backcolor    = 0x252525FF;
+    ts.multiline    = 0;
+
+    if (songlist)
+    {
+	tg_text_add(songlist);
+	tg_text_set(songlist, "SONGS", ts);
+    }
+    else
+	zc_log_debug("songlistbck not found");
+
+    ui.songlisttable = ui_table_create(
+	"songlisttable",
+	songlist,
+	songlistscroll,
+	songlistevt,
+	songlisthead,
+	fields,
+	on_songlist_event);
+
+    /* ui.song_list_data = VNEW(); // REL S0 */
+    /* ui_table_set_data(ui.songlisttable, ui.song_list_data); */
+
+    ui_manager_activate(songlistevt);
 
     // show texture map for debug
 
