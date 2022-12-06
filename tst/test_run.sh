7c65e43933301995e2b3632f12859cd87e5b0c8a
Milan Toth <milgra@milgra.com>
2022-12-06 17:12:17 +0000

    test updates

--- tst/test_run.sh
+++ tst/test_run.sh
@@ -7,16 +7,16 @@ exe="$1/vmp"
 #build/vmp -r res -v -s tst/ui_delete_test/record -l tst/ui_delete_test -c tst/ui_delete_test -f 1200x800 -o
 #build/vmp -r res -v -p tst/ui_delete_test/record -l tst/ui_delete_test -c tst/ui_delete_test -f 1200x800 -o
 
-echo -e "Test metadata editing in a non-organized library\n\n\n"
+echo -e "\nTest metadata editing in a non-organized library\n"
 sh tst/test_rep.sh tst/meta_non_organized $exe
 
-echo -e "Test metadata editing in an organized library\n\n\n"
+echo -e "\nTest metadata editing in an organized library\n"
 sh tst/test_rep.sh tst/meta_organized $exe -o
 
-echo -e "Test non-organized delete\n\n\n"
+echo -e "\nTest non-organized delete\n"
 sh tst/test_rep.sh tst/delete_non_organized $exe
 
-echo -e "Test organized delete\n\n\n"
+echo -e "\nTest organized delete\n"
 sh tst/test_rep.sh tst/delete_organized $exe -o
 
 error=$?
